e0dbcb111385dde85723365d40982567995cb1e2
terryshi <shidakang@yundianjia.com>
2016-11-10 19:40:07 +0800

    finish list

--- lib/list.rb
+++ lib/list.rb
@@ -4,10 +4,28 @@ require 'find'
 class List
   attr_accessor :project
 
+  def initialize(value)
+    self.project = value
+  end
+
   def list_all
-  Find.find("../projects/") do |filename|
-    puts File.basename(filename,'.yml')
+      Find.find("../projects/") do |filename|
+      puts File.basename(filename,'.yml')
+    end
   end
+
+  #find project and list records
+  def list_project(value)
+    Find.find("../projects/") do |filename|
+      if File.basename(filename,'.yml') == value
+        #load yaml file
+        projects =  YAML::load (File.open("../projects/#{value}.yml"))
+        projects.each do |item|
+          puts item
+        end
+      end
+    end
   end
+
 
 end
